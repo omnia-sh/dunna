ed5cf62c93da8f437edac1b3e4b63677dfe00afe
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-23 00:20:36 +0300

    refactor: testing

--- test/location/country.test.ts
+++ test/location/country.test.ts
@@ -5,4 +5,16 @@ describe('Test dunna.country() function', () => {
   it('return a valid country from the list', () => {
     expect(countries).toContainEqual(dunna.country());
   });
+
+  it('return a valid country code', () => {
+    const countriesCodes = countries.map(item => item.ISO2);
+
+    expect(countriesCodes).toContain(dunna.countryCode());
+  });
+
+  it('return a valid country name', () => {
+    const countriesNames = countries.map(item => item.name);
+
+    expect(countriesNames).toContain(dunna.countryName());
+  });
 });
