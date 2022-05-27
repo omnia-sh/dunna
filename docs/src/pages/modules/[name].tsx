78febbabda404ff88ceeb4c5abcd12b7c8fc3545
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-27 21:25:00 +0300

    fix: helpers functions

--- docs/src/pages/modules/[name].tsx
+++ docs/src/pages/modules/[name].tsx
@@ -1,12 +1,22 @@
+import type { GetStaticPaths, GetStaticProps } from 'next';
 import { Module } from '@/types';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { getModuleFromSlug, getNames, getSlugFromName } from '@/utils/helpers';
 
-export const getStaticPaths: GetStaticPaths = (ctx) => {};
+export const getStaticPaths: GetStaticPaths = (ctx) => {
+  return {
+    paths: getNames().map((name) => ({ params: { name } })),
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps = (ctx) => {
-  console.log(ctx.params);
+  const name = ctx.params?.name as string;
+  const slug = getSlugFromName(name);
 
-  return { props: {} };
+  // eslint-disable-next-line @next/next/no-assign-module-variable
+  const module = getModuleFromSlug(slug);
+
+  return { props: { module } };
 };
 
 interface ModuleProps {
@@ -16,7 +26,7 @@ interface ModuleProps {
 const Module = ({ module }: ModuleProps) => {
   return (
     <div>
-      <h1>{}</h1>
+      <h1>{module.title}</h1>
     </div>
   );
 };
--- docs/src/utils/helpers.ts
+++ docs/src/utils/helpers.ts
@@ -6,27 +6,45 @@ import { Module } from '@/types';
 
 const MODULES_PATH = path.join(process.cwd(), 'src/modules');
 
-export function getModuleSlugs() {
+export function getSlugs() {
   const slugs = glob.sync(`${MODULES_PATH}/*.mdx`);
 
   return slugs;
 }
 
-function getModuleFromSlug(slug: string): Module {
-  const source = fs.readFileSync(slug);
+export function getNameFromSlug(slug: string): string {
   const name = slug.split('/').at(-1)?.split('.')[0] as string;
+
+  return name;
+}
+
+export function getSlugFromName(name: string): string {
+  const slug = path.join(MODULES_PATH, `${name}.mdx`);
+
+  return slug;
+}
+
+export function getNames(): string[] {
+  const names = getSlugs().map(getNameFromSlug);
+
+  return names;
+}
+
+export function getModuleFromSlug(slug: string): Module {
+  const source = fs.readFileSync(slug);
+  const name = getNameFromSlug(slug);
 
   const { content, data } = matter(source);
 
   return {
-    content,
     name,
     title: data.title,
+    content,
   };
 }
 
 export function getAllModules(): Module[] {
-  const slugs = getModuleSlugs();
+  const slugs = getSlugs();
 
   const modules = slugs.map(getModuleFromSlug);
 
