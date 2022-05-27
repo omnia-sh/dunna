37a0773215f6a5acbe7cffd3d62e9fac35369844
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-27 21:43:23 +0300

    parse mdx and show it

--- docs/src/pages/modules/[name].tsx
+++ docs/src/pages/modules/[name].tsx
@@ -1,4 +1,7 @@
 import type { GetStaticPaths, GetStaticProps } from 'next';
+import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
 import { Module } from '@/types';
 import { getModuleFromSlug, getNames, getSlugFromName } from '@/utils/helpers';
 
@@ -9,24 +12,35 @@ export const getStaticPaths: GetStaticPaths = (ctx) => {
   };
 };
 
-export const getStaticProps: GetStaticProps = (ctx) => {
+export const getStaticProps: GetStaticProps = async (ctx) => {
   const name = ctx.params?.name as string;
   const slug = getSlugFromName(name);
 
   // eslint-disable-next-line @next/next/no-assign-module-variable
   const module = getModuleFromSlug(slug);
 
-  return { props: { module } };
+  return {
+    props: {
+      module: {
+        ...module,
+        content: await serialize(module.content),
+      },
+    },
+  };
 };
 
 interface ModuleProps {
-  module: Module;
+  module: Module & {
+    content: MDXRemoteSerializeResult<Record<string, unknown>>;
+  };
 }
 
 const Module = ({ module }: ModuleProps) => {
   return (
     <div>
       <h1>{module.title}</h1>
+
+      <MDXRemote {...module.content} />
     </div>
   );
 };
