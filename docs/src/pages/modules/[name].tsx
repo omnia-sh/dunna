39a998ef846ff5bfd4af25075361d081cc2a7cb1
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-27 22:04:11 +0300

    add: code highlight

--- docs/src/pages/modules/[name].tsx
+++ docs/src/pages/modules/[name].tsx
@@ -2,8 +2,12 @@ import type { GetStaticPaths, GetStaticProps } from 'next';
 import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeHighlight from 'rehype-highlight';
 import { Module } from '@/types';
 import { getModuleFromSlug, getNames, getSlugFromName } from '@/utils/helpers';
+import 'highlight.js/styles/atom-one-dark.css';
 
 export const getStaticPaths: GetStaticPaths = (ctx) => {
   return {
@@ -18,12 +22,21 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   // eslint-disable-next-line @next/next/no-assign-module-variable
   const module = getModuleFromSlug(slug);
+  const content = await serialize(module.content, {
+    mdxOptions: {
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+        rehypeHighlight,
+      ],
+    },
+  });
 
   return {
     props: {
       module: {
         ...module,
-        content: await serialize(module.content),
+        content,
       },
     },
   };
