1a83cb6465f719051731f17b2f86de4c547aa1bb
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-28 15:13:38 +0300

    add: TableOfContent for each /docs/[name].tsx

--- docs/src/pages/docs/[name].tsx
+++ docs/src/pages/docs/[name].tsx
@@ -1,6 +1,7 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import type { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
-import { Anchor, Container, Text, Title } from '@mantine/core';
+import { Anchor, Box, Text, Title } from '@mantine/core';
 import { Prism } from '@mantine/prism';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
@@ -11,6 +12,7 @@ import {
   getSlugs,
 } from '@/utils';
 import type { Doc } from '@/types';
+import { TableOfContent } from '@/components';
 
 export const getStaticPaths: GetStaticPaths = (ctx) => {
   return {
@@ -36,49 +38,53 @@ interface DocPageProps {
 
 const DocPage = ({ doc }: DocPageProps) => {
   return (
-    <Container>
-      <Title>{doc.title}</Title>
+    <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 5fr' }}>
+      <TableOfContent active={doc.name} />
 
-      <ReactMarkdown
-        rehypePlugins={[
-          rehypeSlug,
-          [rehypeAutolinkHeadings, { behaviour: 'wrap' }],
-        ]}
-        components={{
-          h1({ children }) {
-            return <Title>{children}</Title>;
-          },
+      <Box>
+        <Title>{doc.title}</Title>
 
-          a({ children, href }) {
-            return (
-              <Anchor sx={{ fontSize: 'inherit' }} href={href}>
-                {children}
-              </Anchor>
-            );
-          },
+        <ReactMarkdown
+          rehypePlugins={[
+            rehypeSlug,
+            [rehypeAutolinkHeadings, { behaviour: 'wrap' }],
+          ]}
+          components={{
+            h1({ children }) {
+              return <Title>{children}</Title>;
+            },
 
-          p({ children }) {
-            return <Text>{children}</Text>;
-          },
+            a({ children, href }) {
+              return (
+                <Link passHref href={href as string}>
+                  <Anchor sx={{ fontSize: 'inherit' }}>{children}</Anchor>
+                </Link>
+              );
+            },
 
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
+            p({ children }) {
+              return <Text>{children}</Text>;
+            },
 
-            return !inline && match ? (
-              <Prism language={match[1] as any}>
-                {String(children).replace(/\n$/, '')}
-              </Prism>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      >
-        {doc.source}
-      </ReactMarkdown>
-    </Container>
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+
+              return !inline && match ? (
+                <Prism language={match[1] as any}>
+                  {String(children).replace(/\n$/, '')}
+                </Prism>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {doc.source}
+        </ReactMarkdown>
+      </Box>
+    </Box>
   );
 };
 
