0dfb1f865a57133bf551028f3830a80eca9902bf
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-28 15:23:14 +0300

    enhance TableOfContent

--- docs/src/components/TableOfContent/TableOfContent.tsx
+++ docs/src/components/TableOfContent/TableOfContent.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Box, ScrollArea } from '@mantine/core';
 import useStyles from './TableOfContent.styles';
 import links from './links';
@@ -10,25 +11,25 @@ const TableOfContent = ({ active }: TableOfContentProps) => {
   const { classes, cx } = useStyles();
 
   const items = links.map((item) => (
-    <Box<'a'>
-      component="a"
-      href={item.link}
-      onClick={(event) => event.preventDefault()}
-      key={item.label}
-      className={cx(classes.link, {
-        [classes.linkActive]: active === item.link,
-      })}
-      sx={(theme) => ({
-        paddingLeft: item.order * theme.spacing.md,
-        fontFamily: 'Roboto',
-      })}
-    >
-      {item.label}
-    </Box>
+    <Link key={item.label} href={item.link} passHref>
+      <Box<'a'>
+        component="a"
+        href={item.link}
+        className={cx(classes.link, {
+          [classes.linkActive]: active === item.link,
+        })}
+        sx={(theme) => ({
+          paddingLeft: item.order * theme.spacing.md,
+          fontFamily: 'Roboto',
+        })}
+      >
+        {item.label}
+      </Box>
+    </Link>
   ));
 
   return (
-    <Box>
+    <Box sx={{ position: 'sticky', top: 0, left: 0 }}>
       <ScrollArea sx={{ height: '100vh' }}>{items}</ScrollArea>
     </Box>
   );
