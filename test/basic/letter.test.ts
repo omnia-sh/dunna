931017227ba8d78578249a3f0a26f66db5b11c6d
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-06-28 16:51:11 +0300

    fix: make first letter small in all it functions

--- test/basic/letter.test.ts
+++ test/basic/letter.test.ts
@@ -2,19 +2,19 @@ import dunna from '../../src';
 import { letters } from '../../src/database';
 
 describe('Test dunna.letter() function', () => {
-  it('Return a letter', () => {
+  it('return a letter', () => {
     const letter = dunna.letter();
 
     expect(letters.all).toContain(letter);
   });
 
-  it('Return an upper case letter', () => {
+  it('return an upper case letter', () => {
     const letter = dunna.letter({ casing: 'upper' });
 
     expect(letters.upper).toContain(letter);
   });
 
-  it('Return a lower case letter', () => {
+  it('return a lower case letter', () => {
     const letter = dunna.letter({ casing: 'lower' });
 
     expect(letters.lower).toContain(letter);
