f9aa4870ddf1c73f220bf4617be1181d7fe15aa3
Ali <aliammarali125@gmail.com>
2022-06-29 16:47:17 +0300

    fix hex start with #

--- src/modules/color/rgb.ts
+++ src/modules/color/rgb.ts
@@ -1,21 +1,24 @@
+import { RGB } from '../../types';
+import hex from './hex';
 
-import { RGB } from "../../types";
-import hex from "./hex";
-
-function rgb():RGB {
-    return hexToRgb(hex());
-  }
-  
-  function hexToRgb(hex:string) :RGB{
-    var bigint = parseInt(hex, 16);
-    var red = (bigint >> 16) & 255;
-    var green = (bigint >> 8) & 255;
-    var blue = bigint & 255;
-
-    return {
-        red,green,blue
-    };
+function rgb(): RGB {
+  return hexToRgb(hex());
 }
 
+function hexToRgb(hex: string): RGB {
+  if (hex.startsWith('#')) {
+    hex = hex.substring(1);
+  }
+  var bigint = parseInt(hex, 16);
+  var red = (bigint >> 16) & 255;
+  var green = (bigint >> 8) & 255;
+  var blue = bigint & 255;
 
-export  { rgb,hexToRgb };
+  return {
+    red,
+    green,
+    blue,
+  };
+}
+
+export { rgb, hexToRgb };
