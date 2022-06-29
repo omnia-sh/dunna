1f3554109f2f35c78272ff3c92a675a27d1b1be7
Ali <aliammarali125@gmail.com>
2022-06-29 16:55:12 +0300

    export colorName

--- src/modules/color/index.ts
+++ src/modules/color/index.ts
@@ -1,5 +1,7 @@
 import  color  from './color';
 import  {rgb}  from './rgb';
 import  hex  from './hex';
+import  colorName  from './color-name';
 
-export {  color , hex , rgb }
+
+export {  color , hex , rgb , colorName }
