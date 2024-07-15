c6d92eec29ad8c9dec1a38b3ad2e674d1902ac43
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2024-07-15 16:21:00 +0300

    docs: init a new docs template

---
+++ docs/astro.config.mjs
@@ -0,0 +1,37 @@
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Dunna",
+      social: {
+        github: "https://github.com/alimehasin/dunna",
+      },
+
+      expressiveCode: {
+        styleOverrides: { borderRadius: "0.25rem" },
+      },
+
+      sidebar: [
+        {
+          label: "Get Started",
+          link: "/get-started",
+        },
+        {
+          label: "Modules",
+          autogenerate: {
+            directory: "modules",
+          },
+        },
+        {
+          label: "Database",
+          autogenerate: {
+            directory: "database",
+          },
+        },
+      ],
+    }),
+  ],
+});
---
+++ docs/src/content/docs/database/person.md
@@ -0,0 +1,6 @@
+---
+title: Person Database
+description: This is a reference page for the person database
+---
+
+This is a reference page for the person database.
